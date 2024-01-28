2cc5854508c92414b836a70fa5ea787c311d38bb
peacekeeper1901 <peacekeeper1901@gmail.com>
2024-01-28 19:28:58 +0600

    feat:persistently add download button to YouTube pages

--- public/background.js
+++ public/background.js
@@ -1,50 +1,9 @@
-// background.js
-// import axios from 'axios';
-// // Функция для загрузки видео
-// const fetchVideoUrl = async (videoUrl, audioOnly = false) => {
-  // try {
-  //   const response = await axios.post('https://co.wuk.sh/api/json', {
-  //     url: encodeURI(videoUrl),
-  //     vQuality: 'max',
-  //     filenamePattern: 'basic',
-  //     isAudioOnly: audioOnly,
-  //     disableMetadata: true,
-  //   }, {
-  //     headers: {
-  //       'Accept': 'application/json', // Добавляем заголовок Accept
-  //     }
-  //   });
-  //   if (response.data && response.data.url) {
-  //     return response.data.url;
-  //   } else {
-  //     throw new Error('No URL found');
-  //   }
-  // } catch (error) {
-  //   console.error(error);
-  //   return null;
-  // }
-// };
-
-// // Принимаем сообщения от контентного скрипта
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//   if (request.action === 'downloadVideo') {
-//     const { url } = request;
-//     fetchVideoUrl(url).then(downloadUrl => {
-//       sendResponse({ downloadUrl });
-//     });
-//     return true;
-//   }
-// });
-
-
-
-
-//Listen for when a Tab changes state
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-  if(changeInfo && changeInfo.status == "complete"){
-    chrome.tabs.sendMessage(tabId, {data: tab}, function(response) {
-        console.log(response);
-    });
+  if (changeInfo && changeInfo.status == "complete"){
+    chrome.tabs.sendMessage(tabId, { urlChanged: true });
   }
 });
 
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+  chrome.tabs.sendMessage(details.tabId, { urlChanged: true });
+});
